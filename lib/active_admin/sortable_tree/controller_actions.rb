5e55dfa48a9bb784a2ed7c1646cb6666a4174228
Shannon Skipper <shannonskipper@gmail.com>
2017-06-08 14:54:40 -0700

    nest module namespaces explicitly for order-independent loading

--- lib/active_admin/sortable_tree/controller_actions.rb
+++ lib/active_admin/sortable_tree/controller_actions.rb
@@ -1,55 +1,57 @@
-module ActiveAdmin::SortableTree
-  module ControllerActions
+module ActiveAdmin
+  module SortableTree
+    module ControllerActions
 
-    attr_accessor :sortable_options
+      attr_accessor :sortable_options
 
-    def sortable(options = {})
-      options.reverse_merge! :sorting_attribute => :position,
-                             :parent_method => :parent,
-                             :children_method => :children,
-                             :roots_method => :roots,
-                             :tree => false,
-                             :max_levels => 0,
-                             :protect_root => false,
-                             :collapsible => false, #hides +/- buttons
-                             :start_collapsed => false,
-                             :sortable => true
+      def sortable(options = {})
+        options.reverse_merge! :sorting_attribute => :position,
+                               :parent_method => :parent,
+                               :children_method => :children,
+                               :roots_method => :roots,
+                               :tree => false,
+                               :max_levels => 0,
+                               :protect_root => false,
+                               :collapsible => false, #hides +/- buttons
+                               :start_collapsed => false,
+                               :sortable => true
 
-      # BAD BAD BAD FIXME: don't pollute original class
-      @sortable_options = options
+        # BAD BAD BAD FIXME: don't pollute original class
+        @sortable_options = options
 
-      # disable pagination
-      config.paginate = false
+        # disable pagination
+        config.paginate = false
 
-      collection_action :sort, :method => :post do
-        resource_name = ActiveAdmin::SortableTree::Compatibility.normalized_resource_name(active_admin_config.resource_name)
+        collection_action :sort, :method => :post do
+          resource_name = ActiveAdmin::SortableTree::Compatibility.normalized_resource_name(active_admin_config.resource_name)
 
-        records = []
-        params[resource_name].each_pair do |resource, parent_resource|
-          parent_resource = resource_class.find(parent_resource) rescue nil
-          records << [resource_class.find(resource), parent_resource]
-        end
+          records = []
+          params[resource_name].each_pair do |resource, parent_resource|
+            parent_resource = resource_class.find(parent_resource) rescue nil
+            records << [resource_class.find(resource), parent_resource]
+          end
 
-        errors = []
-        ActiveRecord::Base.transaction do
-          records.each_with_index do |(record, parent_record), position|
-            record.send "#{options[:sorting_attribute]}=", position
-            if options[:tree]
-              record.send "#{options[:parent_method]}=", parent_record
+          errors = []
+          ActiveRecord::Base.transaction do
+            records.each_with_index do |(record, parent_record), position|
+              record.send "#{options[:sorting_attribute]}=", position
+              if options[:tree]
+                record.send "#{options[:parent_method]}=", parent_record
+              end
+              errors << {record.id => record.errors} if !record.save
             end
-            errors << {record.id => record.errors} if !record.save
+          end
+          if errors.empty?
+            head 200
+          else
+            render json: errors, status: 422
           end
         end
-        if errors.empty?
-          head 200
-        else
-          render json: errors, status: 422
-        end
+
       end
 
     end
 
+    ::ActiveAdmin::ResourceDSL.send(:include, ControllerActions)
   end
-
-  ::ActiveAdmin::ResourceDSL.send(:include, ControllerActions)
 end
